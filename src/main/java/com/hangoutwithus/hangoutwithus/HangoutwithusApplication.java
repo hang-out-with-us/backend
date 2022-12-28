0dbba35cb811aaf5f31201a0e9ef1d2eccaad76a
BottleMoon <gwonbj.dev@gmail.com>
2022-12-28 20:27:19 +0900

    init project

---
+++ src/main/java/com/hangoutwithus/hangoutwithus/HangoutwithusApplication.java
@@ -0,0 +1,13 @@
+package com.hangoutwithus.hangoutwithus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutwithusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangoutwithusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hangoutwithus/hangoutwithus/HangoutwithusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangoutwithus.hangoutwithus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutwithusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

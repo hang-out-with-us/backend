a570d26512fcf4344ccc34a97f9cd1fe8618b7f6
BottleMoon <gwonbj.dev@gmail.com>
2023-05-14 13:58:46 +0900

    Fix token refresh not working - JwtFilter block request when request comes to permitted path with invaild token -> if token is null or empty then ignore it - chage inheritance GenericFilterBean -> OncePerRequestFilter - when token is expired or invaild response error message to client

--- src/main/java/com/hangoutwithus/hangoutwithus/jwt/JwtFilter.java
+++ src/main/java/com/hangoutwithus/hangoutwithus/jwt/JwtFilter.java
@@ -1,10 +1,14 @@
 package com.hangoutwithus.hangoutwithus.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends OncePerRequestFilter {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String REFRESH_TOKEN_HEADER = "RefreshToken";
@@ -27,34 +31,43 @@ public class JwtFilter extends GenericFilterBean {
 
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String jwt = resolveToken(httpServletRequest);
-        String refreshToken = httpServletRequest.getHeader(REFRESH_TOKEN_HEADER);
-        String requestURI = httpServletRequest.getRequestURI();
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = resolveToken(request);
+        String refreshToken = request.getHeader(REFRESH_TOKEN_HEADER);
+        String requestURI = request.getRequestURI();
+
         TokenValidState accessTokenValidState = jwtTokenProvider.validateToken(jwt);
         TokenValidState refreshTokenValidState = jwtTokenProvider.validateToken(refreshToken);
+
+        if((jwt == null || jwt.isEmpty()) && (refreshToken == null || refreshToken.isEmpty())) {
+            log.info("JWT 토큰이 없습니다. uri: {}", requestURI);
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         if (refreshTokenValidState == TokenValidState.VALIDATED &&
                 accessTokenValidState == TokenValidState.VALIDATED) {
             //refreshToken과 jwt 토큰이 모두 유효한 경우
             Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.debug("Security Context에 '{}' 인증 정보를 저장했습니다. uri: {}", authentication.getName(), requestURI);
+            log.info("Security Context에 '{}' 인증 정보를 저장했습니다. uri: {}", authentication.getName(), requestURI);
+            filterChain.doFilter(request, response);
         }
         else if (refreshTokenValidState == TokenValidState.VALIDATED &&
                 accessTokenValidState == TokenValidState.EXPIRED) {
             //refreshToken이 유효하고, jwt 토큰이 만료된 경우
-            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "EXPIRED_ACCESS_TOKEN");
+            log.info("JWT 토큰이 만료되었습니다. uri: {}", requestURI);
+            response.getWriter().write("EXPIRED_ACCESS_TOKEN");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
         }
         else {
             //refreshToken이 유효하지 않거나, jwt 토큰이 유효하지 않은 경우
-            log.debug("유효한 JWT 토큰이 없습니다. uri: {}", requestURI);
-        }
+            log.info("유효한 JWT 토큰이 없습니다. uri: {}", requestURI);
+            response.getWriter().write("INVALID_TOKEN");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
-        chain.doFilter(request, httpServletResponse);
+        }
     }
 
     //Request Header에서 토큰 정보를 추출
